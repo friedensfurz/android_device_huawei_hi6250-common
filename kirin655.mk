5c661c4d97c56f543d2bea06f4652c5d2c2cc753
friedensfurz <24854256+friedensfurz@users.noreply.github.com>
2019-02-05 21:18:40 +0100

    Revert "hi6250-common: LineageOS > AOSP"
    This reverts commit a7360785c4ecddb1aef1a857d9c3aea919bb037b.

--- kirin655.mk
+++ kirin655.mk
@@ -30,6 +30,11 @@ $(call inherit-product-if-exists, vendor/huawei/hi6250-common/hi6250-common-vend
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+ifeq ($(TARGET_AOSP_BASED),)
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage
+endif
+
 # Audio
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:system/etc/usb_audio_policy_configuration.xml
@@ -96,6 +101,10 @@ PRODUCT_PACKAGES += \
 # Release tools
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/releasetools/releasetools.hi6250.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/releasetools.hi6250.sh
+
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
 
 # Selinux
 PRODUCT_COPY_FILES += \
